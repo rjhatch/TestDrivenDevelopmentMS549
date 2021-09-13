24710b63cc69a281320abbaefdb9a61cd8b6d141
richie hatch <rjh378@gmail.com>
2021-09-13 02:15:26 -0600

    Added the menu and associated functionality.

---
+++ TestDrivenDevelopmentMS549/Menu.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopmentMS549
+{
+    class Menu
+    {
+        private int input;
+        public int Input { get { return input; } }
+
+        public bool InitialPrompt()
+        {
+            Console.Write("How many elements would you like to work with (must be an integer from 1 to 10)? ");
+            if (!TestInput(Console.ReadLine(), 1, 10))
+            {
+                ErrorMessage();
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool DisplayMenu()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.Write("1) Add a value.\n" +
+                "2) See the top value.\n" +
+                "3) Remove the top value.\n" +
+                "4) Start over.\n" +
+                "5) Quit.\n>>");
+
+            string choice = Console.ReadLine();
+            if (TestInput(choice, 1, 5))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool TestInput(string value, int min, int max)
+        {
+            try
+            {
+                int num = Int32.Parse(value);
+                if (num >= min && num <= max)
+                {
+                    input = num;
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            return false;
+        }
+
+        public void ErrorMessage()
+        {
+            Console.WriteLine("The value must be a postive integer and within range.");
+        }
+
+        public bool NumberInput()
+        {
+            Console.Write("Enter a number from 1 to 100: ");
+            string choice = Console.ReadLine();
+            if (TestInput(choice, 1, 100))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SuccessMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void FailedMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
--- TestDrivenDevelopmentMS549/Program.cs
+++ TestDrivenDevelopmentMS549/Program.cs
@@ -10,6 +10,74 @@ namespace TestDrivenDevelopmentMS549
     {
         static void Main(string[] args)
         {
+            bool quit = false;
+            bool startOver = false;
+            Menu menu = new Menu();
+
+            while (!quit)
+            {
+                startOver = false;
+
+                if (menu.InitialPrompt())
+                {
+                    StackRH stack = new StackRH(menu.Input);
+
+                    while (!startOver)
+                    {
+                        if (menu.DisplayMenu())
+                        {
+                            if (menu.Input == 1 && menu.NumberInput())
+                            {
+                                if (stack.Push(menu.Input))
+                                {
+                                    menu.SuccessMessage($"Successfully added {menu.Input}!");
+                                }
+                                else
+                                {
+                                    menu.FailedMessage("The stack is full.");
+                                }
+                            }
+                            else if (menu.Input == 2)
+                            {
+                                if (stack.Peek() != null)
+                                {
+                                    menu.SuccessMessage($"The top value is {stack.Peek()}.");
+                                }
+                                else
+                                {
+                                    menu.FailedMessage("The stack is empty.");
+                                }
+                            }
+                            else if (menu.Input == 3)
+                            {
+                                int? success = stack.Pop();
+                                if (success != null)
+                                {
+                                    menu.SuccessMessage($"The removed value is {success}.");
+                                }
+                                else
+                                {
+                                    menu.FailedMessage("The stack is empty.");
+                                }
+                            }
+                            else if (menu.Input == 4)
+                            {
+                                startOver = true;
+                                break;
+                            }
+                            else if (menu.Input == 5)
+                            {
+                                quit = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            menu.ErrorMessage();
+                        }
+                    }
+                }
+            }
         }
     }
 }
