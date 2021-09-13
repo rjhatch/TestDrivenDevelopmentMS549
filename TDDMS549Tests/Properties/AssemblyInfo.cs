39b7e11ddabc20b1d3ba8a75a55584fb36f5c255
richie hatch <rjh378@gmail.com>
2021-09-12 23:38:36 -0600

    Add project files.

---
+++ TDDMS549Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDMS549Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDDMS549Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1331eef3-8aac-4631-8733-49d13b6954fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TDDMS549Tests/TDDMS549Tests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDMS549Tests
+{
+    [TestClass]
+    public class TDDMS549Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StackRH myStack;
+        }
+    }
+}
